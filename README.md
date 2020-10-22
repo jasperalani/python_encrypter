e0fb51d1f489e1fee027e1da052b747bc5336f35
Jasper Alani <jasperalani@gmail.com>
2020-10-22 22:13:14 +0100

    jumble_string and solve_string function, update README.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Python string encoder
+
+```python
+bad = jumble_string('python humble jumble')
+# jumbled string: qxugpm itnamd ktnamd
+
+good = solve_string(bad)
+# solved string: python humble jumble
+```
--- python_encrypter.py
+++ python_encrypter.py
@@ -1,10 +1,10 @@
-# if char has even index in string and is not z its added as its ascii number + 1
+# if char has even index in string and is not z its added as its ascii number + 1, unless it is b then - 1
 # if it has odd index in string and its not a its added as ascii number - 1
 
-def encode_string(string):
+def jumble_string(regular_string):
 	char_list = ''
 	count = 0
-	for char in string:
+	for char in regular_string:
 		if ' ' == char:
 			char_list += ' '
 			continue
@@ -13,8 +13,30 @@ def encode_string(string):
 			if (ascii+1) < 123:
 				new_char = chr(ascii+1)
 		else:
-			if (ascii-1) > 96:
-				new_char = chr(ascii-1)
+		    if (ascii-1) >= 97:
+		        new_char = chr(ascii-1)
+		    else:
+		        new_char = chr(ascii+1)
 		char_list += str(new_char)
 		count += 1
 	return char_list
+
+def solve_string(jumbled_string):
+    char_list = ''
+    count = 0
+    for char in jumbled_string:
+        if ' ' == char:
+            char_list += ' '
+            continue
+        ascii = ord(char)
+        if (count % 2) == 0:
+            if (ascii-1) > 96:
+                new_char = chr(ascii-1)
+        else:
+            if ascii == 98:
+                new_char = chr(ascii-1)
+            elif (ascii+1) < 123:
+                new_char = chr(ascii+1)
+        char_list += str(new_char)
+        count += 1
+    return char_list
